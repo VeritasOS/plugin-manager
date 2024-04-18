8fed71ef2275f2c5373a575807c1445e6f47da8e
Abhijith D.A <abhijith.da@gmail.com>
2024-04-17 23:32:49 -0700

    Revert "Update action and rollback background color."
    This reverts commit b34543e6216f78141b7acb9d837ec3dc8cd20243.

--- graph/graph.go
+++ graph/graph.go
@@ -47,7 +47,7 @@ func prepareSubGraphName(pluginType string) string {
 }
 
 // InitGraph initliazes the graph data structure and invokes generateGraph.
-func InitGraph(pluginType string, pluginsInfo map[string]*pluginmanager.PluginAttributes, options map[string]string) error {
+func InitGraph(pluginType string, pluginsInfo map[string]*pluginmanager.PluginAttributes) error {
 	var err error
 	if graph1 == nil {
 		graph1, err = gv.Graph()
@@ -60,11 +60,7 @@ func InitGraph(pluginType string, pluginsInfo map[string]*pluginmanager.PluginAt
 	sb := graph1.SubGraph(prepareSubGraphName(pluginType), 1)
 	sb.SetLabel(pluginType)
 	sb.Attr(0, "cluster", "true")
-	if val, ok := options["TYPE"]; ok {
-		color := getDisplayColor(val)
-		// sb.SetBackgroundColor("#dae8fc:#7ea6e0")
-		sb.SetBackgroundColor(color)
-	}
+	// sb.SetBackgroundColor("red")
 	sb.SetStyle(cgraph.FilledGraphStyle + "," + cgraph.RoundedGraphStyle)
 	sb.SetGradientAngle(270)
 
@@ -158,23 +154,18 @@ func GenerateGraph() error {
 	return nil
 }
 
-// getDisplayColor returns the color for a given result status.
-func getDisplayColor(key string) string {
+// getStatusColor returns the color for a given result status.
+func getStatusColor(status string) string {
 	// Node color
-	color := "#dae8fc:#7ea6e0" // blue // dStatusStart by default
-	switch key {
-	case "ACTION":
-		color = "#d5e8d4:#ffffff"
-	case "ROLLBACK":
-		color = "#f8cecc:#ffffff"
-	case pluginmanager.DStatusFail:
-		color = "#f8cecc:#ea6b66" // "red"
-	case pluginmanager.DStatusOk:
-		color = "#d5e8d4:#97d077" // "green"
-	case pluginmanager.DStatusSkip:
-		color = "yellow"
+	ncolor := "#dae8fc:#7ea6e0" // blue // dStatusStart by default
+	if status == pluginmanager.DStatusFail {
+		ncolor = "#f8cecc:#ea6b66" // "red"
+	} else if status == pluginmanager.DStatusOk {
+		ncolor = "#d5e8d4:#97d077" // "green"
+	} else if status == pluginmanager.DStatusSkip {
+		ncolor = "yellow"
 	}
-	return color
+	return ncolor
 }
 
 // UpdateGraph updates the plugin node with the status and url.
@@ -192,7 +183,7 @@ func UpdateGraph(subgraphName, plugin, status, url string) error {
 		return err
 	}
 	node.SetStyle("filled")
-	node.SetFillColor(getDisplayColor(status))
+	node.SetFillColor(getStatusColor(status))
 	if url != "" {
 		node.SetURL(url)
 	}
